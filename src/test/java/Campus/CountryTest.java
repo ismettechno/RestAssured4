d1b9914772ce06ba5ab25c1aba960a52b475acac
TechnoStudy <ismettechno@gmail.com>
2023-01-12 08:41:38 +0000

    RESTAssured 5

--- src/test/java/Campus/CountryTest.java
+++ src/test/java/Campus/CountryTest.java
@@ -20,44 +20,72 @@ public class CountryTest {
     String countryCode;
 
     @BeforeClass
-    public void loginCampus()
-    {
-        baseURI="https://test.mersys.io/";
+    public void loginCampus() {
+        baseURI = "https://test.mersys.io/";
         //diğer testler çalışmadan önce login olup cookies i alınması gerekiyor
         // bu yüzden beforeClass annotation ı  eklendi
 
-        Map<String,String> credential=new HashMap<>();
+        Map<String, String> credential = new HashMap<>();
         credential.put("username", "turkeyts");
         credential.put("password", "TechnoStudy123");
         credential.put("rememberMe", "true");
 
-        cookies=
-        given()
-                .contentType(ContentType.JSON)
-                .body(credential)
+        cookies =
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(credential)
 
-                .when()
-                .post("auth/login")
+                        .when()
+                        .post("auth/login")
 
-                .then()
-                //.log().body()
-                .statusCode(200)
-                .extract().response().getDetailedCookies()
+                        .then()
+                        //.log().body()
+                        .statusCode(200)
+                        .extract().response().getDetailedCookies()
         ;
 
-       // System.out.println("cookies = " + cookies);
+        // System.out.println("cookies = " + cookies);
     }
 
-    public String getRandomName() {  return RandomStringUtils.randomAlphabetic(8); }
-    public String getRandomCode() {  return RandomStringUtils.randomAlphabetic(3); }
+    public String getRandomName() {
+        return RandomStringUtils.randomAlphabetic(8);
+    }
+
+    public String getRandomCode() {
+        return RandomStringUtils.randomAlphabetic(3);
+    }
 
 
     @Test
-    public void createCountry()
-    {
-        Country country=new Country();
-        country.setName(getRandomName());
-        country.setCode(getRandomCode());
+    public void createCountry() {
+        countryName = getRandomName();
+        countryCode = getRandomCode();
+
+        Country country = new Country();
+        country.setName(countryName);
+        country.setCode(countryCode);
+
+        countryID =
+                given()
+                        .cookies(cookies)
+                        .contentType(ContentType.JSON)
+                        .body(country)
+
+                        .when()
+                        .post("school-service/api/countries")
+
+                        .then()
+                        .log().body()
+                        .statusCode(201)
+                        .extract().jsonPath().getString("id")
+        ;
+    }
+
+    @Test(dependsOnMethods = "createCountry", priority = 1)
+    public void createCountryNegative() {
+        Country country = new Country();
+        country.setName(countryName);
+        country.setCode(countryCode);
 
         given()
                 .cookies(cookies)
@@ -69,14 +97,95 @@ public class CountryTest {
 
                 .then()
                 .log().body()
-                .statusCode(201)
+                .statusCode(400)
+                .body("message", equalTo("The Country with Name \"" + countryName + "\" already exists."))
+                .body("message", containsString("already exists"))
         ;
     }
 
-    @Test
-    public void createCountryNegative()
-    {
 
+    @Test(dependsOnMethods = "createCountry", priority = 2)
+    public void updateCountry() {
+        countryName = getRandomName();
+        countryCode = getRandomCode();
+
+        Country country = new Country();
+        country.setId(countryID);
+        country.setName(countryName);
+        country.setCode(countryCode);
+
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(country)
+
+                .when()
+                .put("school-service/api/countries")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+                .body("name", equalTo(countryName))
+        ;
+    }
+
+    @Test(dependsOnMethods = "updateCountry")
+    public void deleteCountryById() {
+        given()
+                .cookies(cookies)
+                .pathParam("countryID", countryID)
+                .log().uri()
+
+                .when()
+                .delete("school-service/api/countries/{countryID}")
+
+                .then()
+                .log().body()
+                .statusCode(200)
+        ;
+    }
+
+    @Test(dependsOnMethods = "deleteCountryById")
+    public void deleteCountryByIdNegative() {
+        given()
+                .cookies(cookies)
+                .pathParam("countryID", countryID)
+                .log().uri()
+
+                .when()
+                .delete("school-service/api/countries/{countryID}")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", equalTo("Country not found"))
+        ;
+    }
+
+    // task : Silinmiş bir ülkeyi update etmeye çalışınız : updateCountryNegative
+    @Test(dependsOnMethods = "deleteCountryById")
+    public void updateCountryNegative() {
+        countryName = getRandomName();
+        countryCode = getRandomCode();
+
+        Country country = new Country();
+        country.setId(countryID);
+        country.setName(countryName);
+        country.setCode(countryCode);
+
+        given()
+                .cookies(cookies)
+                .contentType(ContentType.JSON)
+                .body(country)
+
+                .when()
+                .put("school-service/api/countries")
+
+                .then()
+                .log().body()
+                .statusCode(400)
+                .body("message", equalTo("Country not found"))
+        ;
     }
 
 
